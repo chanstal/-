8a8640dfa5b25f093e84c42ff4f048f3fcd0c4c7
chanstal <43198566+chanstal@users.noreply.github.com>
2018-11-21 18:52:44 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        float n=0.0f;
+        float minVal=Float.MAX_VALUE;
+        float maxVal=Float.MIN_VALUE;
+        for(int i=0;i<10;i++ ){
+            n=scn.nextFloat();
+            if(n<minVal){
+                minVal=n;
+            }
+            if(n>maxVal){
+                maxVal=n;
+            }
+        }
+        System.out.format("max=%.2f\n",maxVal);
+        System.out.format("min=%.2f\n",minVal);
+
+
+    }
+}

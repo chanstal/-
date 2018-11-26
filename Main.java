30bb55d218fc8f025b62f0e1687ec52b1f727d75
chanstal <43198566+chanstal@users.noreply.github.com>
2018-11-26 20:08:13 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int sum=0,i=0;
+        while (sum<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println(i-1);
+    }
+}

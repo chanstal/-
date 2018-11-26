518a72d0d8a06252b84ca629527b4ff34036c02f
chanstal <43198566+chanstal@users.noreply.github.com>
2018-11-26 13:47:08 +0800

    Update Q18 數字串和.java

--- Q18 數字串和.java
+++ Q18 數字串和.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
-        int i=n,sum=0,k=0;
-        while (i>=1){
-            sum+=i%10;
+        int sum=0,k=0;
+        while (n>=1){
+            sum+=n%10;
             k++;
-            i=i/10;
+            n=n/10;
 
         }
         System.out.println(k);
